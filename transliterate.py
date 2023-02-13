da8815d3898a796644f2c5ddc2ad110d1b26667d
Dhaivatv <vipatdhaivat25@gmail.com>
2023-02-13 20:58:02 +0530

    transliterateion module added

--- transliterate.py
+++ transliterate.py
@@ -1,12 +1,7 @@
 from google.transliteration import transliterate_word
 
 def transliterate(text):
-    result = transliterate_word('''Phir le aaya dil majboor kya keeje
-Raas na aaya rehna door kya keeje
-Dil keh raha use maqammal kar bhi aao
-Wo jo adhoori si baat baaki hai
-Wo jo adhoori si yaad baaki hai
-Wo jo adhoori si yaad baaki hai''', lang_code='hi')
-    return result
+    result = transliterate_word(text, lang_code='hi')
+    return result[0]
 
 
