b2fd382d61610e254b6369c78f54af714a016313
Dhaivatv <vipatdhaivat25@gmail.com>
2023-02-13 16:41:56 +0530

    claasification_api created

--- api/app.py
+++ api/app.py
@@ -1,6 +1,12 @@
 from flask import Flask
 from flask import request
+import pickle
+import pandas as pd
+import numpy as np
+
 app = Flask(__name__)
+
+model = pickle.load(open('mood_classification.pkl', 'rb'))
 
 @app.route("/")
 def hello():
@@ -9,8 +15,11 @@ def hello():
 @app.route('/get-lyrics', methods = ['POST'])
 def get_lyrics():
     data = request.get_json()
-    print(data)
-    return 0
+    lyric_pred= data["lyrics"]
+    daa=np.array([lyric_pred])
+    ser=pd.Series(daa,index=[0])
+    y_pred = str(model.predict(ser)[0])
+    return {"mood": y_pred}
 
 if __name__ == "__main__":
     app.run()
