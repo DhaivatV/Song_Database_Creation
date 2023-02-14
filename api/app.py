6dfe52cc4475cbdca86276fbcd05cfca46fa41ee
Dhaivatv <vipatdhaivat25@gmail.com>
2023-02-14 20:22:03 +0530

    transliteration integrated with api

--- api/app.py
+++ api/app.py
@@ -3,7 +3,7 @@ from flask import request
 import pickle
 import pandas as pd
 import numpy as np
-from api.transliterate import Transliterate
+from transliterate import Transliterate
 
 app = Flask(__name__)
 
@@ -11,13 +11,12 @@ model = pickle.load(open('mood_classification.pkl', 'rb'))
 
 @app.route("/")
 def hello():
-    return "Hey Stranger"
+    return "Hey Listener"
 
 @app.route('/get-lyrics-hindi', methods = ['POST'])
 def get_lyrics():
     data = request.get_json()
     lyric_pred= Transliterate(data["lyrics"])
-    print(lyric_pred)
     daa=np.array([lyric_pred])
     ser=pd.Series(daa,index=[0])
     y_pred = str(model.predict(ser)[0])
--- api/transliterate.py
+++ api/transliterate.py
@@ -5,3 +5,4 @@ def Transliterate(text):
     return result[0]
 
 
+# print(Transliterate("I found a love, for me Darling, just dive right in and follow my lead  Well, I found a girl, beautiful and sweet Oh, I never knew you were the someone waiting for me"))
